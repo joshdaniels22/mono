09023796e956a4f4fac831a2e6acfea172b7e8eb
Joshua Daniels <joshuadaniels@Joshuas-MacBook-Air.local>
2024-04-27 23:35:05 +0200

    update config setup script

--- config/show_commands.sh
+++ config/show_commands.sh
@@ -6,5 +6,5 @@ echo "# Copy the following to your terminal rc file (bashrc/zshrc)"
 echo
 echo '#####################################################'
 echo '# Custom Config Files'
-echo source $(pwd)/.config
+echo source $(pwd)/.config.sh
 echo '#####################################################'
